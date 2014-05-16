f9cb2e91343292693e02e44f742302f2791ed43d
Winston Hoy (Curiouser) <winston@curiousercreative.com>
2014-05-16 14:35:41 -0400

    sample preferences file, remove the -sample

---
+++ preferences-sample.php
@@ -0,0 +1,8 @@
+<?php
+
+$preferences->db->host = ''; // host name of database server
+$preferences->db->user = '';
+$preferences->db->pass = '';
+$preferences->db->name = ''; // name of database
+
+?>
